fa923e49dd5af8fbdb7d0383d8f63817f572ff39
TheComputerM <mudit.somani00@gmail.com>
2024-10-07 02:21:57 +0530

    add content collections

--- website/app.config.ts
+++ website/app.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from "@solidjs/start/config";
 import tsconfigPaths from 'vite-tsconfig-paths'
-
+import contentCollections from "@content-collections/solid-start";
 
 export default defineConfig({
   vite: {
-    plugins: [tsconfigPaths()]
+    plugins: [tsconfigPaths(), contentCollections()]
   }
 });
