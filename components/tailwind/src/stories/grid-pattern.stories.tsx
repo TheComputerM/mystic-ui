70ce960efba646276e751d92eba772f881a79cd8
TheComputerM <mudit.somani00@gmail.com>
2024-10-06 14:55:32 +0530

    add biome linter

--- components/tailwind/src/stories/grid-pattern.stories.tsx
+++ components/tailwind/src/stories/grid-pattern.stories.tsx
@@ -3,16 +3,16 @@ import type { Meta, StoryObj } from "storybook-solidjs";
 import { GridPattern } from "../ui/grid-pattern";
 
 const meta: Meta<typeof GridPattern> = {
-  title: "Grid Pattern",
-  component: GridPattern,
-  tags: ["autodocs"],
-  argTypes: {
-    width: { control: "number" },
-    height: { control: "number" },
-    x: { control: "number" },
-    y: { control: "number" },
-    strokeDasharray: { control: "text" }
-  },
+	title: "Grid Pattern",
+	component: GridPattern,
+	tags: ["autodocs"],
+	argTypes: {
+		width: { control: "number" },
+		height: { control: "number" },
+		x: { control: "number" },
+		y: { control: "number" },
+		strokeDasharray: { control: "text" },
+	},
 };
 
 export default meta;
--- components/tailwind/src/ui/grid-pattern.tsx
+++ components/tailwind/src/ui/grid-pattern.tsx
@@ -1,71 +1,71 @@
-import { createUniqueId, For, Show } from "solid-js";
+import { For, Show, createUniqueId } from "solid-js";
 
 import { cn } from "@/lib/utils";
 
 interface GridPatternProps {
-  width?: number;
-  height?: number;
-  x?: number;
-  y?: number;
-  squares?: Array<[x: number, y: number]>;
-  strokeDasharray?: string;
-  class?: string;
-  [key: string]: unknown;
+	width?: number;
+	height?: number;
+	x?: number;
+	y?: number;
+	squares?: Array<[x: number, y: number]>;
+	strokeDasharray?: string;
+	class?: string;
+	[key: string]: unknown;
 }
 
 export function GridPattern({
-  width = 40,
-  height = 40,
-  x = -1,
-  y = -1,
-  strokeDasharray = "0",
-  squares,
-  class: className,
-  ...props
+	width = 40,
+	height = 40,
+	x = -1,
+	y = -1,
+	strokeDasharray = "0",
+	squares,
+	class: className,
+	...props
 }: GridPatternProps) {
-  const id = createUniqueId();
+	const id = createUniqueId();
 
-  return (
-    <svg
-      aria-hidden="true"
-      class={cn(
-        "pointer-events-none absolute inset-0 h-full w-full fill-gray-400/30 stroke-gray-400/30",
-        className
-      )}
-      {...props}
-    >
-      <defs>
-        <pattern
-          id={id}
-          width={width}
-          height={height}
-          patternUnits="userSpaceOnUse"
-          x={x}
-          y={y}
-        >
-          <path
-            d={`M.5 ${height}V.5H${width}`}
-            fill="none"
-            stroke-dasharray={strokeDasharray}
-          />
-        </pattern>
-      </defs>
-      <rect width="100%" height="100%" stroke-width={0} fill={`url(#${id})`} />
-      <Show when={squares}>
-        <svg x={x} y={y} class="overflow-visible">
-          <For each={squares}>
-            {([x, y]) => (
-              <rect
-                stroke-width="0"
-                width={width - 1}
-                height={height - 1}
-                x={x * width + 1}
-                y={y * height + 1}
-              />
-            )}
-          </For>
-        </svg>
-      </Show>
-    </svg>
-  );
+	return (
+		<svg
+			aria-hidden="true"
+			class={cn(
+				"pointer-events-none absolute inset-0 h-full w-full fill-gray-400/30 stroke-gray-400/30",
+				className,
+			)}
+			{...props}
+		>
+			<defs>
+				<pattern
+					id={id}
+					width={width}
+					height={height}
+					patternUnits="userSpaceOnUse"
+					x={x}
+					y={y}
+				>
+					<path
+						d={`M.5 ${height}V.5H${width}`}
+						fill="none"
+						stroke-dasharray={strokeDasharray}
+					/>
+				</pattern>
+			</defs>
+			<rect width="100%" height="100%" stroke-width={0} fill={`url(#${id})`} />
+			<Show when={squares}>
+				<svg x={x} y={y} class="overflow-visible" aria-hidden="true">
+					<For each={squares}>
+						{([x, y]) => (
+							<rect
+								stroke-width="0"
+								width={width - 1}
+								height={height - 1}
+								x={x * width + 1}
+								y={y * height + 1}
+							/>
+						)}
+					</For>
+				</svg>
+			</Show>
+		</svg>
+	);
 }
