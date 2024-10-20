5f9ab18c890b6371a181fb93c80a676782a53a88
TheComputerM <mudit.somani00@gmail.com>
2024-10-20 10:49:06 +0530

    build(website): pause mdx generation

--- website/content-collections.ts
+++ website/content-collections.ts
@@ -3,7 +3,7 @@ import { compile } from "@mdx-js/mdx";
 import path from "node:path";
 import { writeFile, mkdir } from "node:fs/promises";
 
-import remarkGfm from 'remark-gfm'
+import remarkGfm from "remark-gfm";
 import rehypeSlug from "rehype-slug";
 
 await mkdir("./.content-collections/generated/docs", { recursive: true });
@@ -16,30 +16,30 @@ const docs = defineCollection({
 		description: z.string(),
 	}),
 	async transform(document, context) {
-		const jsxPath = await context.cache(document.content, async (content) => {
-			const vfile = await compile(content, {
-				jsx: true,
-				jsxImportSource: "solid-js/h",
-				providerImportSource: "~/tools/solid-mdx.ts",
-				remarkPlugins: [remarkGfm],
-				rehypePlugins: [rehypeSlug],
-				elementAttributeNameCase: "html",
-				outputFormat: "program",
-				development: process.env.NODE_ENV === "development",
-				baseUrl: path.join(
-					context.collection.directory,
-					document._meta.filePath,
-				),
-			});
-			const output = String(vfile);
-			const filePath = `./.content-collections/generated/${context.collection.name}/${document._meta.fileName.replace(".mdx", ".jsx")}`;
-			await writeFile(filePath, output);
-			return filePath;
-		});
+		// const jsxPath = await context.cache(document.content, async (content) => {
+		// 	const vfile = await compile(content, {
+		// 		jsx: true,
+		// 		jsxImportSource: "solid-js/h",
+		// 		providerImportSource: "~/tools/solid-mdx.ts",
+		// 		remarkPlugins: [remarkGfm],
+		// 		rehypePlugins: [rehypeSlug],
+		// 		elementAttributeNameCase: "html",
+		// 		outputFormat: "program",
+		// 		development: process.env.NODE_ENV === "development",
+		// 		baseUrl: path.join(
+		// 			context.collection.directory,
+		// 			document._meta.filePath,
+		// 		),
+		// 	});
+		// 	const output = String(vfile);
+		// 	const filePath = `./.content-collections/generated/${context.collection.name}/${document._meta.fileName.replace(".mdx", ".jsx")}`;
+		// 	await writeFile(filePath, output);
+		// 	return filePath;
+		// });
 
 		return {
 			...document,
-			jsxPath,
+			// jsxPath,
 		};
 	},
 });
