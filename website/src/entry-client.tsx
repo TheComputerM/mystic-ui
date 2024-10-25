0fd4671804c2fc32a0d146dcc98e862f22c82518
TheComputerM <mudit.somani00@gmail.com>
2024-10-26 02:58:23 +0530

    chore: update biome config

--- website/src/entry-client.tsx
+++ website/src/entry-client.tsx
@@ -1,5 +1,4 @@
 // @refresh reload
 import { StartClient, mount } from "@solidjs/start/client";
 
-// biome-ignore lint/style/noNonNullAssertion: brehga
 mount(() => <StartClient />, document.getElementById("app")!);
